b33abc3ac3c5de363b83439d95bfdf2467e4ac73
anoy369 <adannoy369@gmail.com>
2022-08-16 02:04:32 +0600

    removed messurement-id from firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -14,8 +14,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_ESSEGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MESSUREMENT_ID
+  appId: process.env.REACT_APP_APP_ID
 };
 
 
