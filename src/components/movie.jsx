4fc8618d390350a385431963c77cb93b106f5257
anoy369 <adannoy369@gmail.com>
2022-08-15 12:23:28 +0600

    configure environment for firebase

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -7,11 +7,18 @@ const Movie = ({item}) => {
 
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-1">
-      <img
-        className="w-full h-auto block rounded-[3px]"
-        src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-        alt={item?.title}
-      />
+      {item?.backdrop_path ? (  
+        <img 
+          className="w-full h-auto block rounded-[3px]"
+          src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+          alt={item?.title}
+        />):(
+        <img 
+          className="w-full h-auto block rounded-[3px]"
+          src={`https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png`}
+          alt={item?.title}
+        />
+        )}
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
         <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
           {item?.title}
